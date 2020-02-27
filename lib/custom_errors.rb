15d31ca04e6343317380cf5146f9024828898450
James Batsell <jamesbatsell05@gmail.com>
2020-02-27 20:27:29 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,14 +5,14 @@ class Person
     @name = name
   end
 
-  def get_married(person)
+ def get_married(person)
     self.partner = person
     if person.class != Person
       raise PartnerError
     else
       person.partner = self
+    end
   end
-end
   
   class PartnerError < StanndardError
   end
